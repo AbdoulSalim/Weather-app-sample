10e0229bf20e4d974f3745366d59f88b5709c389
Abdoul Salim <gabdoulsalim@live.fr>
2020-01-07 13:22:56 +0100

    install styled-components and set the folder structure

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Importing StyledNavbar
+import StyledNavbar from './StyledNavbar';
+
+const Navbar = () => {
+  return(
+    <div>
+      <StyledNavbar>
+        This is the nav bar
+      </StyledNavbar>
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import StyledSearchBar from './StyledSearchBar'
+
+const SearchBar = () => {
+  return(
+    <div>
+      <StyledSearchBar>
+        This is the search bar
+      </StyledSearchBar>
+    </div>
+  )
+}
+
+export default SearchBar;
